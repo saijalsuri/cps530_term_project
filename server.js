6f9b45eac233b8fafb2cb504421f85179c6279a1
saijalsuri <saijalsuri@live.com>
2018-12-02 02:36:31 -0500

    Initial Commit!

--- server.js
+++ server.js
@@ -1,7 +1,26 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const app = express();
-const port = process.env.PORT || 5000;
+var express = require('express');
+var path = require('path');
+var bodyParser = require('body-parser');
+var cors = require('cors')
+var index = require('./routes/index');
+// app
+var app = express();
+// cors
+app.use(cors());
+// views. -- VERY IMPORTANT 
+app.set('views', path.join(__dirname, 'src'));
+// engine 
+app.set('view enginer', 'ejs');
+app.engine('html', require('ejs').renderFile);
+// angular  dist -- VERY IMPORTANT
+app.use(express.static(__dirname + '/dist'));
+// body bodyParser
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.listen(port, () => console.log(`Listening on port ${port}`));
+app.use(bodyParser.urlencoded({extended: false}));
+// routes
+app.use('/', index);
+// port.
+var server = app.listen(process.env.PORT || 8080, function () {
+var port = server.address().port;
+console.log("App now running on port", port);
+});
