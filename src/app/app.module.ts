d8df4c806178995bae7a3ad19fa777833a358f3c
saijalsuri <saijalsuri@live.com>
2018-12-01 22:51:10 -0500

    Initial Commit!

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { InstallComponent } from './install/install.component';
+import { ExampleComponent } from './example/example.component';
+import { ExperienceComponent } from './experience/experience.component';
+import { ContributionComponent } from './contribution/contribution.component';
+import { AboutComponent } from './about/about.component';
+import { TutorialComponent } from './tutorial/tutorial.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    InstallComponent,
+    ExampleComponent,
+    ExperienceComponent,
+    ContributionComponent,
+    AboutComponent,
+    TutorialComponent
   ],
   imports: [
     BrowserModule,
