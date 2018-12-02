1790cbff8adf12a5974e64770a2469648a6f294c
saijalsuri <saijalsuri@live.com>
2018-12-02 00:32:39 -0500

    Initial Commit!

--- server.js
+++ server.js
@@ -1,10 +1,5 @@
 var express = require("express");
 var bodyParser = require("body-parser");
-var mongodb = require("mongodb");
-var ObjectID = mongodb.ObjectID;
-
-var COMMENTS_COLLECTION = "comments";
-
 var app = express();
 app.use(bodyParser.json());
 var distDir = __dirname + "/dist/";
