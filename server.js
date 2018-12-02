a1252d86f18f7424391f6927c8a000304e165e45
saijalsuri <saijalsuri@live.com>
2018-12-02 01:05:18 -0500

    Initial Commit!

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
-var express = require("express");
-var bodyParser = require("body-parser");
-var app = express();
-app.use(bodyParser.json());
-var distDir = __dirname + "dist/cps530meanstackapp";
-app.use(express.static(distDir));
-app.listen(process.env.PORT || 3000);
+const http = require('http');
+const PORT = process.env.PORT || 5000;
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World\n');
+});
+server.listen(PORT, () => {
+  console.log(`Server running on ${PORT}/`);
+});
