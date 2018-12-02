2143122b73e294a701580fd3ca7624f83a4110ed
saijalsuri <saijalsuri@live.com>
2018-12-02 00:10:23 -0500

    Initial Commit!

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require('path');
 const app = express();
 app.use(express.static(__dirname + '/dist/cps530meanstackapp'));
 app.get('/*', function(req,res) {
-res.sendFile(path.join(__dirname+'/dist/cps530meanstackapp/main.html'));
+res.sendFile(path.join(__dirname+'/dist/cps530meanstackapp/index.html'));
 
 });
 app.listen(process.env.PORT || 3000);
