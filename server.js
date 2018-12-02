47abbe1649cd5cb5d88c4550b670c6765c778f10
saijalsuri <saijalsuri@live.com>
2018-12-02 02:13:43 -0500

    Initial Commit!

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var app = express();
 var path = require('path');
 
 
-app.use(express.static(path.join(__dirname, 'src')));
+app.use(express.static(path.join(__dirname, 'app')));
 
     var server = app.listen(process.env.PORT || 5000), function() {
         var port = server.address().port;
