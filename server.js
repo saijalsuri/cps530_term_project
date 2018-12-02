d8ff92c4b3ce5ba5e594fa023171acf837a44b04
saijalsuri <saijalsuri@live.com>
2018-12-02 01:11:15 -0500

    Initial Commit!

--- server.js
+++ server.js
@@ -1,10 +1,6 @@
 const http = require('http');
 const PORT = process.env.PORT || 5000;
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/html');
-  res.end('Hello World\n');
-});
+const server = http.createServer(req, res);
 server.listen(PORT, () => {
   console.log(`Server running on ${PORT}/`);
 });
