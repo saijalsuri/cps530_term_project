7051ed8e381edb0a13d8d4870322db488c63a48f
saijalsuri <saijalsuri@live.com>
2018-12-02 00:02:25 -0500

    Initial Commit!

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require('path');
 const app = express();
 app.use(express.static(__dirname + '/dist/frontend'));
 app.get('/*', function(req,res) {
-res.sendFile(path.join(__dirname+'/dist/frontend/index.html'));
+res.sendFile(path.join(__dirname+'/dist/frontend/main.html'));
 
 });
 app.listen(process.env.PORT || 3000);
