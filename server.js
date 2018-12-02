c2b3f246a46e8796d6c7fbc136ec99a83c51d87b
saijalsuri <saijalsuri@live.com>
2018-12-02 01:02:06 -0500

    Initial Commit!

--- server.js
+++ server.js
@@ -2,6 +2,6 @@ var express = require("express");
 var bodyParser = require("body-parser");
 var app = express();
 app.use(bodyParser.json());
-var distDir = __dirname + "dist/";
+var distDir = __dirname + "dist/cps530meanstackapp";
 app.use(express.static(distDir));
 app.listen(process.env.PORT || 3000);
