47b778472e8ebb003c07a244c9506a8d0eee2b29
saijalsuri <saijalsuri@live.com>
2018-12-02 17:12:07 -0500

    add routing URLs

--- src/app/contribution/connect.js
+++ src/app/contribution/connect.js
@@ -1,12 +1,12 @@
-const mongo = require('mongodb').MongoClient
+const mongo = require('mongodb').MongoClient;
 function doConnect() {
- 	var url = 'mongodb:/admin:givemeaccess01@ds113849.mlab.com:13849/mydb'
+ 	var url = 'mongodb:/admin:givemeaccess01@ds113849.mlab.com:13849/mydb';
 	mongo.connect(url, (err, db) => {
 		if(err) { 
-			throw err
+			throw err;
 		} else {
-			console.log('Successfully connected to MongoDB')
+			console.log('Successfully connected to MongoDB');
 		}
 	})
 }
-doConnect()
+doConnect();
