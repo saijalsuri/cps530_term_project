6218e06ece127d415a04fcce3e702c67c6c306f1
saijalsuri <saijalsuri@live.com>
2018-12-02 00:20:52 -0500

    Initial Commit!

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/frontend'));
+app.use(express.static(__dirname + '/dist/cps530meanstackapp'));
 app.get('/*', function(req,res) {
-res.sendFile(path.join(__dirname+'/dist/frontend/index.html'));
+res.sendFile(path.join(__dirname+'/dist/cps530meanstackapp/index.html'));
 
 });
 app.listen(process.env.PORT || 3000);
