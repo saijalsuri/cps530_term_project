3e52253503b00399e133a3182eea9bbf9b7f2c38
saijalsuri <saijalsuri@live.com>
2018-12-02 03:04:04 -0500

    Initial Commit!

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
 router.get('/', function(req, res, next) {
-   res.render('../src/app/about/about.component.html');
+   res.render('../src/app/about/app.component.html');
 });
 module.exports = router;
