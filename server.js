fba490fbef170ff1194c50721d0a077819f1da52
saijalsuri <saijalsuri@live.com>
2018-12-02 20:47:14 -0500

    connecting to dlab

--- server.js
+++ server.js
@@ -60,8 +60,7 @@ app.post("/createComment", (req, res) => {
   var myData = new Comment(req.body);
   myData.save()
     .then(item => {
-      res.sendFile(path.join(__dirname + '/dist/frontend/'));
-      console.log("The new comment was successfully saved to the database!");
+      res.send("Thank you for your message! It has been recorded in our MongoDB database and we will respond ASAP!");
     })
     .catch(err => {
       res.status(400).send("unable to save to database");
