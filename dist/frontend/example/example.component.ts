6edaa1f67939534c0111b598418e46639ffd27c0
saijalsuri <saijalsuri@live.com>
2018-12-02 22:47:16 -0500

    final

--- dist/frontend/example/example.component.ts
+++ dist/frontend/example/example.component.ts
@@ -1,11 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { Example } from '../example';
 
 @Component({
   selector: 'app-example',
-  templateUrl: './example.component.html',
+  templateUrl: './example.component.html'
 })
 export class ExampleComponent implements OnInit {
-
+	
+  example: Example = {
+    id: 1,
+    name: 'Example_name'
+  };
+  
   constructor() { }
 
   ngOnInit() {
