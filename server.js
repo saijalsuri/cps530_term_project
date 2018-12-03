1d87372c359631f4a197421702ea9cc381a7075f
saijalsuri <saijalsuri@live.com>
2018-12-02 20:39:15 -0500

    connecting to dlab

--- server.js
+++ server.js
@@ -60,7 +60,7 @@ app.post("/createComment", (req, res) => {
   var myData = new Comment(req.body);
   myData.save()
     .then(item => {
-      res.sendFile(path.join(__dirname + '/dist/frontend'));
+      res.sendFile(path.join(__dirname + '/dist/frontend/contribution/'));
       console.log("The new comment was successfully saved to the database!");
     })
     .catch(err => {
